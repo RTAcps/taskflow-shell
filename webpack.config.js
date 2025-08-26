a64328ce2c776e6e1b0369ad133e1a1cad922413
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2025-08-26 18:52:59 -0300

    refactor: adjust taskflow-shell prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,11 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-// Detecta se está em produção pelo env NODE_ENV ou variável customizada
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = withModuleFederationPlugin({
   remotes: {
-    "taskflow-component": isProd
-      ? "taskflow-component@https://taskflow-component.netlify.app/remoteEntry.js"
-      : "taskflow-component@http://localhost:4201/remoteEntry.js",
-    "taskflow-reactive": isProd
-      ? "taskflow-reactive@https://taskflow-reactive.netlify.app/remoteEntry.js"
-      : "taskflow-reactive@http://localhost:4202/remoteEntry.js",
-    "taskflow-functional": isProd
-      ? "taskflow-functional@https://taskflow-functional.netlify.app/remoteEntry.js"
-      : "taskflow-functional@http://localhost:4203/remoteEntry.js",
+    "taskflow-component": "taskflow-component@http://localhost:4201/remoteEntry.js",
+    "taskflow-reactive": "taskflow-reactive@http://localhost:4202/remoteEntry.js",
+    "taskflow-functional": "taskflow-functional@http://localhost:4203/remoteEntry.js",
   },
-
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
