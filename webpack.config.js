de3c4e1adfd0c4cc91e379ef8d167300b4c2cd36
RODRIGO TEIXEIRA DE ANDRADE <62621434+RTAcps@users.noreply.github.com>
2025-08-26 17:06:15 -0300

    Update webpack config for production remote entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-module.exports = withModuleFederationPlugin({
+// Detecta se está em produção pelo env NODE_ENV ou variável customizada
+const isProd = process.env.NODE_ENV === 'production';
+const componentForm = "taskflow-component": isProd
+      ? "taskflow-component@https://taskflow-component.netlify.app/remoteEntry.js"
+      : "taskflow-component@http://localhost:4201/remoteEntry.js
+const reactiveForm = "taskflow-reactive": isProd
+      ? "taskflow-reactive@https://taskflow-reactive.netlify.app/remoteEntry.js"
+      : "taskflow-reactive@http://localhost:4202/remoteEntry.js"
+const functionalFrom = "taskflow-functional": isProd
+      ? "taskflow-functional@https://taskflow-functional.netlify.app/remoteEntry.js"
+      : "taskflow-functional@http://localhost:4203/remoteEntry.js"
 
+module.exports = withModuleFederationPlugin({
   remotes: {
-    "taskflow-component": "http://localhost:4201/remoteEntry.js",    
-    "taskflow-reactive": "http://localhost:4202/remoteEntry.js",    
-    "taskflow-functional": "http://localhost:4203/remoteEntry.js",    
+    componentForm,
+    reactiveForm,
+    functionalFrom,
   },
 
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
-
 });
