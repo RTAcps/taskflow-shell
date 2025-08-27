564f23d05e00ffc1231843d572a8bb67763a1df1
RODRIGO TEIXEIRA DE ANDRADE <62621434+RTAcps@users.noreply.github.com>
2025-08-27 07:31:06 -0300

    Update remote URLs for taskflow components

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 
 module.exports = withModuleFederationPlugin({
   remotes: {
-    "taskflow-component": "taskflow-component@http://localhost:4201/remoteEntry.js",
-    "taskflow-reactive": "taskflow-reactive@http://localhost:4202/remoteEntry.js",
-    "taskflow-functional": "taskflow-functional@http://localhost:4203/remoteEntry.js",
+    "taskflow-component": "taskflow-component@https://taskflow-component.netlify.app/remoteEntry.js",
+    "taskflow-reactive": "taskflow-reactive@https://taskflow-reactive.netlify.app/remoteEntry.js",
+    "taskflow-functional": "taskflow-functional@https://taskflow-functional.netlify.app/remoteEntry.js",
   },
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
