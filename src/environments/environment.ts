5c7882453106563e0700cee7196d36d7bfe05bf8
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2025-08-30 08:46:59 -0300

    refactor: force to use remote applications

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     remoteUrls: {
-        'taskflow-component': 'http://localhost:4201/remoteEntry.js',
-        'taskflow-reactive': 'http://localhost:4202/remoteEntry.js',
-        'taskflow-functional': 'http://localhost:4203/remoteEntry.js',
+        'taskflow-component': 'https://taskflow-component.netlify.app/remoteEntry.js',
+        'taskflow-reactive': 'https://taskflow-reactive.netlify.app/remoteEntry.js',
+        'taskflow-functional': 'https://taskflow-functional.netlify.app/remoteEntry.js',
     }
 };
