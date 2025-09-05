a124738f6bcc50809b315c9950e624f115a90395
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2025-09-04 22:50:18 -0300

    refactor: adjust routes mfes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,6 @@ import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MfeDiagnosticsComponent } from './shared/diagnostics/mfe-diagnostics.component';
-import { ModuleUnavailableComponent } from './shared/module-unavailable/module-unavailable.component';
-
-const loadRemoteComponent = (remoteName: string, exposedModule: string) => {
-  return loadRemoteModule({
-      remoteName: remoteName, 
-      exposedModule: exposedModule
-    })
-    .then(m => m[exposedModule.replace('./', '')]) 
-    .catch(err => {
-      console.error(`Erro ao carregar ${remoteName}/${exposedModule}`, err);
-      return ModuleUnavailableComponent; 
-    });
-};
 
 export const routes: Routes = [
     {
@@ -28,55 +15,27 @@ export const routes: Routes = [
     },
     {
         path: 'project-management',
-        children: [
-            {
-                path: '',
-                redirectTo: 'projects',
-                pathMatch: 'full'
-            },
-            {
-                path: 'projects',
-                loadComponent: () => loadRemoteComponent('taskflow-component', './ProjectListComponent')
-            },
-            {
-                path: 'projects/new',
-                loadComponent: () => loadRemoteComponent('taskflow-component', './ProjectFormComponent')
-            },
-            {
-                path: 'projects/edit/:id',
-                loadComponent: () => loadRemoteComponent('taskflow-component', './ProjectFormComponent')
-            },
-            {
-                path: 'projects/:id/board',
-                loadComponent: () => loadRemoteComponent('taskflow-component', './KanbanBoardComponent')
-            }
-        ]
+        loadComponent: () =>
+            loadRemoteModule({
+                remoteName: 'taskflow-component',
+                exposedModule: './Routes',
+            }).then((c) => c.routes),
     },
     {
         path: 'real-time-collaboration',
-        loadComponent: () => loadRemoteComponent('taskflow-reactive', './RealTimeCollaborationComponent')
+        loadChildren: () =>
+            loadRemoteModule({
+                remoteName: 'taskflow-reactive',
+                exposedModule: './Routes',
+            }).then((c) => c.routes), 
     },
     {
         path: 'analytics-report',
-        children: [
-            {
-                path: '',
-                redirectTo: 'dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'dashboard',
-                loadComponent: () => loadRemoteComponent('taskflow-functional', './AnalyticsReportComponent')
-            },
-            {
-                path: 'reports/new',
-                loadComponent: () => loadRemoteComponent('taskflow-functional', './ReportFormComponent')
-            },
-            {
-                path: 'reports/:id',
-                loadComponent: () => loadRemoteComponent('taskflow-functional', './ReportDetailComponent')
-            }
-        ]
+        loadChildren: () =>
+            loadRemoteModule({
+                remoteName: 'taskflow-functional',
+                exposedModule: './Routes',
+            }).then((c) => c.routes), 
     },
     {
         path: '**',
